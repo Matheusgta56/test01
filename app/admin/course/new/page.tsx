d4d1c39e9e1cc6dbe47580258710c92bf2f05574
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-11-22 00:14:53 +0000

    fix: Imagems form url externa

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,15 +1,24 @@
 import UploadButton from "@/app/components/UploadButton";
 import { sql } from "@vercel/postgres";
+import { useSearchParams } from "next/navigation";
 
 export const revalidate =0
 
-export default function NewCourse() {
+export default function NewCourse({
+    searchParams,
+  }: {
+    searchParams?: {
+      url?: string;  
+    };
+  }){
+    
+    const urlImage = searchParams?.url || '';
+
     async function saveCourse(formData: FormData){
         "use server"
         const title = formData.get("title") as string;
         const description = formData.get("description") as string;
-        const url = formData.get("url") as string;
-        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${url})`
+        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${urlImage})`
         console.log("Acessou a função")
     }
     return (
@@ -18,7 +27,7 @@ export default function NewCourse() {
             <form>
                 <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
                 <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
-                <input type="text" name="url" placeholder="Digite a url da imagem" /><br/><br/>
+                <br/>
                 <UploadButton />
                 <button formAction={saveCourse} className="text-white">Salvar</button>
             </form>
